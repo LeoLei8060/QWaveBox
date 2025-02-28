9e34de9da84010649284dd07ef5fff6172c1b601
LeoLei8060 <18157100608@163.com>
2025-02-28 22:59:42 +0800

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
-#include "mainwindow.h"
+#include "mainWidget.h"
+#include "fontmanager.h"
 
 #include <QApplication>
 #include <QCommandLineParser>
 #include <QFileInfo>
+#include <QDebug>
+
+bool initializeFonts() {
+    if (!FontManager::instance()->addThirdpartyFont(":/resources/iconFont/iconfont.ttf", FontManager::IconFont)) {
+        qWarning() << "Failed to load icon font.";
+        return false;
+    }
+    return true;
+}
+
+bool initializeStyle(QApplication *app) {
+    QFile file("://resources/black.qss");
+    QString stylesheets = "";
+    if (file.open(QFile::ReadOnly)) {
+        stylesheets = QLatin1String(file.readAll());
+        app->setStyleSheet(stylesheets);
+        file.close();
+        return true;
+    }
+    return false;
+}
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
@@ -19,18 +41,23 @@ int main(int argc, char *argv[]) {
     parser.addVersionOption();
     parser.addPositionalArgument("file", "The file to open");
     parser.process(a);
+
+    if (!initializeFonts())
+        return 0;
+    if (!initializeStyle(&a))
+        return 0;
     
-    MainWindow w;
+    MainWidget w;
     w.show();
     
     // 如果命令行中指定了文件，则打开该文件
-    const QStringList args = parser.positionalArguments();
-    if (!args.isEmpty()) {
-        const QString fileName = args.first();
-        if (QFileInfo::exists(fileName)) {
-            w.openFile(fileName);
-        }
-    }
+    // const QStringList args = parser.positionalArguments();
+    // if (!args.isEmpty()) {
+    //     const QString fileName = args.first();
+    //     if (QFileInfo::exists(fileName)) {
+    //         w.openFile(fileName);
+    //     }
+    // }
     
     return a.exec();
 }
