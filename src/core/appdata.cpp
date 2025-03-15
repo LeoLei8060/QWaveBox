72c6eeee5dd1b2dd07b7ffd3ce4e00e0f15ee5c1
LeoLei8060 <18157100608@163.com>
2025-03-15 10:34:43 +0800

    添加底层结构

---
+++ src/core/appdata.cpp
@@ -0,0 +1,52 @@
+#include "appdata.h"
+
+AppData::AppData() {}
+
+void AppData::addPlayFileToDefAlbum(const PlayFile &file)
+{
+    m_defaultAlbum.addPlayFile(file);
+}
+
+void AppData::deletePlayFileFromDefAlbum(const QString &filename)
+{
+    m_defaultAlbum.deletePlayFile(filename);
+}
+
+void AppData::addCustomAlbum(const QString &albumName)
+{
+    m_customAlbums.append(Album(albumName));
+}
+
+void AppData::addPlayFileToCusAlbum(const QString &albumName, const PlayFile &file)
+{
+    for (Album &album : m_customAlbums) {
+        if (album.getAlbumName() == albumName) {
+            album.addPlayFile(file);
+            break;
+        }
+    }
+}
+
+void AppData::deletePlayFileFromCusAlbum(const QString &albumName, const QString &filename)
+{
+    for (Album &album : m_customAlbums) {
+        if (album.getAlbumName() == albumName) {
+            album.deletePlayFile(filename);
+            break;
+        }
+    }
+}
+
+Album::Album(const QString &name)
+    : m_name(name)
+{}
+
+void Album::deletePlayFile(const QString &filename)
+{
+    for (auto iter = m_files.begin(); iter != m_files.end();) {
+        if ((*iter).filename_ == filename)
+            iter = m_files.erase(iter);
+        else
+            ++iter;
+    }
+}
---
+++ src/core/appdata.h
@@ -0,0 +1,68 @@
+#ifndef APPDATA_H
+#define APPDATA_H
+
+#include <obj_conv/reflex_format.hpp>
+#include <QList>
+
+struct PlayFile
+{
+    QString filename_;
+    QString filepath_;
+
+    REFLEX_BIND(A(filename_, "filename"), A(filepath_, "filepath"))
+};
+
+class Album
+{
+public:
+    Album() = default;
+    Album(const QString &name);
+
+    QString getAlbumName() const { return m_name; }
+    void    setAlbumName(const QString &name) { m_name = name; }
+
+    QList<PlayFile> getPlayfiles() const { return m_files; }
+    void            addPlayFile(const PlayFile &file) { m_files.append(file); }
+    void            deletePlayFile(const QString &filename);
+
+private:
+    QString         m_name{"默认专辑"};
+    QList<PlayFile> m_files;
+
+    REFLEX_BIND(A(m_name, "name"), A(m_files, "files"))
+};
+
+class AppData
+{
+public:
+    AppData();
+
+    QString getDefaultAlbumName() { return m_defaultAlbum.getAlbumName(); }
+    Album   getDefaultList() const { return m_defaultAlbum; }
+    void    addPlayFileToDefAlbum(const PlayFile &file);
+    void    deletePlayFileFromDefAlbum(const QString &filename);
+
+    QList<Album> getCustomAlbums() const { return m_customAlbums; }
+    void         addCustomAlbum(const QString &albumName);
+    void         addPlayFileToCusAlbum(const QString &albumName, const PlayFile &file);
+    void         deletePlayFileFromCusAlbum(const QString &albumName, const QString &filename);
+
+    int  getVolume() const { return m_volume; }
+    void setVolume(int volume) { m_volume = volume; }
+
+    bool isMute() const { return m_isMute; }
+    void setMute(bool isMute) { m_isMute = isMute; }
+
+private:
+    Album        m_defaultAlbum;
+    QList<Album> m_customAlbums;
+    int          m_volume{50};
+    bool         m_isMute{false};
+
+    REFLEX_BIND(A(m_defaultAlbum, "defaultAlbum"),
+                A(m_customAlbums, "customAlbums"),
+                A(m_volume, "volume"),
+                A(m_isMute, "isMute"))
+};
+
+#endif // APPDATA_H
